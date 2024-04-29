507512ef3afe96e408181954c2fc4b82e936ebd1
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-04-29 17:47:34 -0500

    1.2.1 Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'SimpleDevelopments'
-version '1.2'
+version '1.2.1'
 description 'Simple Priorities Script that gives you county & city priorities!'
 
 client_scripts {'client/client.lua'}
